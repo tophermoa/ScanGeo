c4323c518013c769e2f00fc6ab75bf93d193241f
tophermoa <moasukasuka@gmail.com>
2022-11-20 22:01:21 +0700

    other changes

--- src/components/CurrentMap.js
+++ src/components/CurrentMap.js
@@ -1,5 +1,10 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
 import Map, {Marker, useMap} from 'react-map-gl';
+import mapboxgl from 'mapbox-gl';
+
+// @ts-ignore
+// eslint-disable-next-line import/no-webpack-loader-syntax
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
 const CurrentMap = (props) =>{
     const HiddenComp = () => {
